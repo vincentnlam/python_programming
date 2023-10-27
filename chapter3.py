374557b7adca0f38b035e7f85becab2491326dca
Vincent Nlam <vincentnlam2011@gmail.com>
2023-10-27 18:52:23 -0400

    practiced strings

---
+++ chapter3.py
@@ -0,0 +1,67 @@
+first_name = 'Vincent'
+last_name = "N'lam"
+name = f"{first_name} {last_name}"
+print(first_name)
+print(last_name)
+print(name)
+print(first_name * 2)
+
+print(f"{name}")
+print(f"{first_name}\t\t{last_name}")
+print("\\")
+
+paragraph = '''first line
+second line
+third line'''
+
+paragraph_2 = """first line
+second line
+third line"""
+
+print(paragraph)
+
+print(paragraph_2)
+
+print("comp" "sci")
+print("computer" + " science")
+print("H20" * 3)
+print("C02" * 0)
+print("They’ll hibernate during the winter.")
+print("\"Absolutely not,\" he said.")
+print("\"He said, \'Absoloutely not,\'\" recalled Mel.")
+print("hydrogen sulfide")
+print("left\\right")
+print('''A
+B
+C''')
+
+# len()
+print(len("Vincent"))
+print(len(""))
+x = 3 
+y = 12.5
+print(f"The rabbit is {x}")
+print(f"The rabbit is {x} years old.")
+print(f"{y} is average.")
+print(f"{y} * {x}")
+print(f"{y} * {x} is 37.5")
+print("g" == "g")
+print("g" == "G")
+print("a" > "b")
+print("ant" < "abc")
+print("ant" > "Ant")
+print("ant" > "Abc")
+print("ant" < "anti")
+for x in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz": 
+    print (x, ord(x), hex(ord(x)))
+# num = input("Enter a number: ")
+# num = float(num)
+# print(num)
+# z = num * 2 
+# print(z)
+print("abc" "def")
+left = 'abc'
+right = 'def'
+print(left, right)
+print(f"{left} {right}")
+print("Hello" * 0)
